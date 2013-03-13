bde1edc37b69560bada356c5c03b3dd032e76c54
jason <644458812@qq.com>
2013-03-14 07:05:59 +0800

    merge the static_pages to master

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,15 +2,27 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
+  let(:base_title) { 'Ruby on Rails Tutorial Sample App' }
+=begin
+  def test_have_selector(selector, _text, path)
+    it "should have the #{selector}' #{text}" do
+      visit path
+      page.should have_selector(selector, text: _text)
+    end
+  end
+=end
   describe "Home page" do
+    #test_have_selector 'h1', 'Sample App', '/static_pages/home'
+
     it "should have the h1 'Sample App'" do
       visit '/static_pages/home'
       page.should have_selector('h1', text: 'Sample App')
     end
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      page.should have_selector('title', text: 'Ruby on Rails Tutorial Sample App | Home')
+      page.should have_selector('title', text: "#{base_title} | Home")
     end
+
   end
 
   describe "Help page" do
@@ -20,7 +32,7 @@ describe "StaticPages" do
     end
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      page.should have_selector('title', text: 'Ruby on Rails Tutorial Sample App | Help')
+      page.should have_selector('title', text: "#{base_title} | Help")
     end
   end
 
@@ -31,7 +43,7 @@ describe "StaticPages" do
     end
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      page.should have_selector('title', text: 'Ruby on Rails Tutorial Sample App | About Us')
+      page.should have_selector('title', text: "#{base_title} | About Us")
     end
   end
 
