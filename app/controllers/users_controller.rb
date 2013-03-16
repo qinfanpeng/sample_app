20fc2e841ab2f5c3d9d05710475e8d6f5a46bdd2
jason <644458812@qq.com>
2013-03-16 17:27:07 +0800

    Finsh user edit, update, index, destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
+# -*- coding: utf-8 -*-
 class UsersController < ApplicationController
+  before_filter :require_sign_in, only: [:edit, :update, :index]
+  before_filter :only_youself, only: [:edit, :update]
+  before_filter :require_admin, only: :destroy
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
   def new
     @user = User.new
   end
@@ -17,4 +26,44 @@ class UsersController < ApplicationController
       render :new
     end
   end
+
+  def edit
+    @user = User.find(params[:id])  # 因为在 only_youself中已经定义了 @user， 所有次行代码可以删去
+  end
+
+  def update
+    @user = User.find(params[:id])  # 同理，次行代码也可删除
+    if @user.update_attributes(params[:user])
+      flash[:success] = "updated your profile"
+      sign_in @user
+      redirect_to @user
+    else
+      flash.now[:error] = "update fail"
+      render :edit
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_path
+  end
+
+  private
+
+    def require_sign_in
+      unless signed_in?
+        store_location
+        redirect_to signin_path, notice: "Please sign in "
+      end
+    end
+
+    def only_youself
+      @user = User.find(params[:id])
+      redirect_to root_url, notice: "you can only edit your profile" unless current_user?(@user)
+    end
+
+    def require_admin
+      redirect_to root_url, notice: "only admin can delete other user " unless current_user.admin?
+    end
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,20 @@
+# -*- coding: utf-8 -*-
 FactoryGirl.define do
+=begin
   factory :user do
     name     "Michael Hartl"
     email    "michael@example.com"
     password "foobar"
     password_confirmation "foobar"
+  end
+=end
+  factory :user do
+    sequence(:name) { |n| "Person #{n}" }  # sequence 方法会生成一系列不重复的 user
+    sequence(:email) { |n| "person_#{n}@example.com"}
+    password "foobar"
+    password_confirmation "foobar"
+    factory :admin do
+      admin true
+    end
   end
 end
